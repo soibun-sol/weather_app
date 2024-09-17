93bfe0d6861945b99766f069f21696162d761d03
Keegan Hinton <khinton386@gmail.com>
2024-09-16 22:30:06 -0400

    created class city and added method that reads searchHistory.json

--- Develop/server/src/service/historyService.ts
+++ Develop/server/src/service/historyService.ts
@@ -1,9 +1,26 @@
+import fs from 'fs';
 // TODO: Define a City class with name and id properties
+class City {
+  id: string;
+  name: string;
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+}
 
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  private async read() {
+    fs.readFile('searchHistory.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      return data;
+    }
+  }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   // private async write(cities: City[]) {}
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects

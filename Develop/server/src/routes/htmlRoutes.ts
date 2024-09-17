eb763c0d282846f4f60dbac73a751aa3e5576ad8
Keegan Hinton <khinton386@gmail.com>
2024-09-16 22:53:17 -0400

    defined route to serve html

--- Develop/server/src/routes/htmlRoutes.ts
+++ Develop/server/src/routes/htmlRoutes.ts
@@ -6,5 +6,9 @@ const __dirname = path.dirname(__filename);
 const router = Router();
 
 // TODO: Define route to serve index.html
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
+
 
 export default router;

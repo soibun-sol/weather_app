3332bdf90a7dc06deba27fdecd34c0e710b30954
Keegan Hinton <khinton386@gmail.com>
2024-09-16 23:19:55 -0400

    added line to serve static file of entire client dist folder

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-
+app.use(express.static('../../client/dist'));
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 
 // TODO: Implement middleware to connect the routes
